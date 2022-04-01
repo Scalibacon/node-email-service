233270aacbb4e04612e814443c75ba60ad4f3c86
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-03-31 21:04:11 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes/routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
