d1734bbff72dc6a02ff45208f0cde945fdf5949a
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-04-07 17:04:40 -0300

    middleware pra bloquear acessos não autorizados

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes/routes';
 
+const corsOptions = {
+  origin: 'https://scalibacon.github.io',
+}
+
+const originBlocker = function(req: Request, res: Response, next: NextFunction) {  
+  const baseURL = req.protocol + '://' + req.headers.host + '/';
+  const reqUrl = new URL(req.url,baseURL);
+
+  if(reqUrl.hostname !== 'https://scalibacon.github.io'){
+    console.log('Tentativa de acesso bloqueada! Host =>', reqUrl.hostname);
+    return res.status(403).send('Invalid origin');
+  }
+
+  return next();
+}
+
 const app = express();
-app.use(cors({
-  origin: ['https://scalibacon.github.io']
-}));
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(originBlocker);
 app.use(routes);
 app.use(errors());
 
