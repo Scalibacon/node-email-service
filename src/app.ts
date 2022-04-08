1efd694da935815731ae863ea277b3c0921d17b9
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-04-07 23:51:56 -0300

    aplicando middleware de secutiry em prod

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
-// app.use(originBlocker);
+app.use(originBlocker);
 app.use(routes);
 app.use(errors());
 
